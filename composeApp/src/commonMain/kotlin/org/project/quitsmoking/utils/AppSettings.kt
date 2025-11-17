8a8b3e2baf13682d7150f40d983cd34e1a1bd0bf
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2025-11-17 21:16:04 +0100

    add datastore settings

---
+++ composeApp/src/commonMain/kotlin/org/project/quitsmoking/utils/AppSettings.kt
@@ -0,0 +1,28 @@
+package org.project.quitsmoking.utils
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import kotlinx.coroutines.InternalCoroutinesApi
+import kotlinx.coroutines.internal.SynchronizedObject
+import kotlinx.coroutines.internal.synchronized
+import okio.Path.Companion.toPath
+object AppSettings {
+    private lateinit var dataStore: DataStore<Preferences>
+
+    @OptIn(InternalCoroutinesApi::class)
+    private val lock = SynchronizedObject()
+
+    @OptIn(InternalCoroutinesApi::class)
+    fun getDataStore(producePath: () -> String): DataStore<Preferences> {
+        return synchronized(lock) {
+            if (AppSettings::dataStore.isInitialized) {
+                dataStore
+            } else {
+                PreferenceDataStoreFactory.createWithPath(
+                    produceFile = { producePath().toPath() }
+                ).also { dataStore = it }
+            }
+        }
+    }
+}
