6e4c06592ac894762bd928bd7729c3b720dab7cb
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2025-11-17 21:15:04 +0100

    add dependencies

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,11 +1,19 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import com.codingfeline.buildkonfig.compiler.FieldSpec
+import java.util.Properties
+import kotlin.apply
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
+
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.composeHotReload)
+
+    alias(libs.plugins.buildkonfig)
+    alias(libs.plugins.mokkery)
 }
 
 kotlin {
@@ -29,16 +37,47 @@ kotlin {
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
+            implementation(libs.koin.android)
+            implementation(libs.koin.androidx.compose)
         }
         commonMain.dependencies {
+            // 🎨 Compose Multiplatform
             implementation(compose.runtime)
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
             implementation(compose.components.resources)
             implementation(compose.components.uiToolingPreview)
+
+            // Lifecycle
+            implementation(libs.androidx.lifecycle.viewmodel)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            // Kotlinx datetime
+            implementation(libs.kotlinx.datetime)
+
+            // Logging
+            implementation(libs.kermit)
+
+            // Koin
+            api(libs.koin.core)
+            implementation(libs.koin.compose)
+            implementation(libs.koin.compose.viewmodel)
+
+            // DataStore
+            implementation(libs.androidx.datastore)
+            implementation(libs.androidx.datastore.preferences)
+
+            // Navigation
+            implementation(libs.androidx.navigation.compose)
+
+            //Material Design 3
+            implementation(compose.material3)
+            implementation(compose.material3AdaptiveNavigationSuite)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.material3AdaptiveNavigationSuite)
+            implementation(libs.material3.adaptive)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -77,3 +116,28 @@ dependencies {
     debugImplementation(compose.uiTooling)
 }
 
+// BuildConfig
+buildkonfig {
+    packageName = "org.project"
+
+    val localProperties =
+        Properties().apply {
+            val propsFile = rootProject.file("local.properties")
+            if (propsFile.exists()) {
+                load(propsFile.inputStream())
+            }
+        }
+
+    defaultConfigs {
+        buildConfigField(
+            FieldSpec.Type.BOOLEAN,
+            "isDebugBuild",
+            localProperties["isDebugBuild"]?.toString(),
+        )
+        buildConfigField(
+            FieldSpec.Type.STRING,
+            "appVersion",
+            localProperties["appVersion"]?.toString(),
+        )
+    }
+}
