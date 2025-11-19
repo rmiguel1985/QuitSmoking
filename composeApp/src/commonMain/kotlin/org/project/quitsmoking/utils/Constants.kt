da1ec5176807c9692fb2fc682dbb94a8bdb5a483
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2025-11-19 21:35:22 +0100

    add constants to manage saved settings in data store

---
+++ composeApp/src/commonMain/kotlin/org/project/quitsmoking/utils/Constants.kt
@@ -0,0 +1,10 @@
+package org.project.quitsmoking.utils
+
+// DataStore
+object PreferenceKeys {
+    const val STOP_DATE = "preferences_stop_date"
+    const val STOP_TIME = "preferences_stop_time"
+    const val CIGARETTES_PER_DAY = "preferences_smoke_habit_cigarettes_per_day"
+    const val TIME_SPENT_PER_CIGARETTE = "preferences_smoke_habit_cigarettes_time"
+    const val COST_PER_CIGARETTE = "preferences_cigarette_cost"
+}
