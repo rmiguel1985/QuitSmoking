3a0dfba56021e2fe46ae90e6abf292b37d314bbc
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2025-12-12 11:50:14 +0100

    add injection

--- composeApp/src/commonMain/kotlin/org/project/quitsmoking/di/UseCaseModule.kt
+++ composeApp/src/commonMain/kotlin/org/project/quitsmoking/di/UseCaseModule.kt
@@ -3,9 +3,14 @@ package org.project.quitsmoking.di
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.project.quitsmoking.features.health.domain.HealthUseCase
+import org.project.quitsmoking.features.health.domain.IHealthUseCase
 import org.project.quitsmoking.features.settings.domain.ISettingsUseCase
 import org.project.quitsmoking.features.settings.domain.SettingsUseCase
+import kotlin.time.ExperimentalTime
 
+@OptIn(ExperimentalTime::class)
 val useCaseModule = module {
     singleOf(::SettingsUseCase) { bind<ISettingsUseCase>() }
+    singleOf(::HealthUseCase) { bind<IHealthUseCase>()  }
 }
