6a26acc0a3f7d7aa61af14ce3ed736f63e02e048
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2025-12-12 10:29:00 +0100

    add missing constants

--- composeApp/src/commonMain/kotlin/org/project/quitsmoking/utils/Constants.kt
+++ composeApp/src/commonMain/kotlin/org/project/quitsmoking/utils/Constants.kt
@@ -2,9 +2,26 @@ package org.project.quitsmoking.utils
 
 // DataStore
 object PreferenceKeys {
+
+    // DataStore
     const val STOP_DATE = "preferences_stop_date"
     const val STOP_TIME = "preferences_stop_time"
     const val CIGARETTES_PER_DAY = "preferences_smoke_habit_cigarettes_per_day"
     const val TIME_SPENT_PER_CIGARETTE = "preferences_smoke_habit_cigarettes_time"
     const val COST_PER_CIGARETTE = "preferences_cigarette_cost"
+
+    // Health
+    const val BLOOD_PRESSURE = 0.0139
+    const val CARBON_MONOXIDE = 0.3333
+    const val HEART_ATTACK = 1.0
+    const val SENSE_OF_SMELL = 2.0
+    const val BREATHING = 3.0
+    const val NICOTINE_WITHDRAWAL = 7.0
+    const val CAPACITY = 90.0
+    const val LUNG_HAIRS = 270.0
+    const val CORONARY_ARTERIES = 365.0
+    const val HEART_ATTACK_2_YEARS = 730.0
+    const val STROKE = 1825.0
+    const val LUNG_CANCER = 3650.0
+    const val HEART_ATTACK_15_YEARS = 5475.0
 }
