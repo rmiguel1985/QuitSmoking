d046da45327363abd7651dc0bc3ef94249d95099
Ruben Miguel Corcoba <ruben.feina@gmail.com>
2026-08-18 22:04:51 +0200

    add use case

--- composeApp/src/commonMain/kotlin/org/project/quitsmoking/di/UseCaseModule.kt
+++ composeApp/src/commonMain/kotlin/org/project/quitsmoking/di/UseCaseModule.kt
@@ -5,6 +5,8 @@ import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import org.project.quitsmoking.features.health.domain.HealthUseCase
 import org.project.quitsmoking.features.health.domain.IHealthUseCase
+import org.project.quitsmoking.features.overview.domain.IOverviewUseCase
+import org.project.quitsmoking.features.overview.domain.OverviewUseCase
 import org.project.quitsmoking.features.settings.domain.ISettingsUseCase
 import org.project.quitsmoking.features.settings.domain.SettingsUseCase
 import kotlin.time.ExperimentalTime
@@ -12,5 +14,6 @@ import kotlin.time.ExperimentalTime
 @OptIn(ExperimentalTime::class)
 val useCaseModule = module {
     singleOf(::SettingsUseCase) { bind<ISettingsUseCase>() }
+    singleOf(::OverviewUseCase) { bind<IOverviewUseCase>() }
     singleOf(::HealthUseCase) { bind<IHealthUseCase>()  }
 }
